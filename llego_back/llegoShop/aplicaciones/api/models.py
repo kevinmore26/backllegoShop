3370c439cfce5eeb0e1cb7d0cc59695f264e29e3
Kevin More <74431425+kevinmore26@users.noreply.github.com>
2022-06-11 23:19:22 -0500

    commit

--- llego_back/llegoShop/aplicaciones/api/models.py
+++ llego_back/llegoShop/aplicaciones/api/models.py
@@ -6,5 +6,9 @@ class Productos (models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=200)
-    stockProducto = models.PositiveIntegerField(max_length=4)
-    productoImage =  models.CharField(max_length=300)
+    stockProducto = models.CharField(max_length=20)
+    productoImagen =  models.CharField(max_length=300)
+    ejemplo = models.CharField(max_length=10,default='hola')
+
+
+    
