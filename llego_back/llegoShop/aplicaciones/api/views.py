854c52c7ebdde62f02bef0bbf4b3b2289d056aff
Kevin More <74431425+kevinmore26@users.noreply.github.com>
2022-06-22 22:21:10 -0500

    COMMIT

--- llego_back/llegoShop/aplicaciones/api/views.py
+++ llego_back/llegoShop/aplicaciones/api/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import render
 from django.views import View
 from .models import Productos
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator 
 import json
-
 # Create your views here.
 
 class ProductoView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request , *args , **kwargs )  :
+        return super().dispatch(request, *args, **kwargs)
     
     def get (self,request):
         productos = list(Productos.objects.values())
@@ -19,4 +23,11 @@ class ProductoView(View):
             
         return JsonResponse(datos)
     
+    def post(self,request):
+        
+        jd = json.loads(request.body)
+        
+        Productos.objects.create(nombre=jd['nombre'],descripcion=jd['descripcion'],stockProducto=jd['stockProducto'],productoImagen=jd['productoImagen'])
+        datos = {'message':"Success"}
+        return JsonResponse(datos)
     
